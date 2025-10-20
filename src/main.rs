f01729b305f9b409d57ea18f418b4f6807239a39
Jacob Curley <jcurley@fnal.gov>
2025-10-20 14:44:36 +0000

    Add CI pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn announce_self() -> String {
+    String::from("Hello, world!")
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", announce_self());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::announce_self;
+
+    #[test]
+    fn test_hello_world() {
+        assert_eq!(announce_self(), String::from("Hello, world!"));
+    }
 }
