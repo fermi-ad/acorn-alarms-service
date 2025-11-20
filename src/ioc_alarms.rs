5d22afd0740c8ade947f11a3a52755a011ce1c49
rnethi1701 <rnethi@fnal.gov>
2025-11-20 13:11:00 -0600

    modified main.rs

--- src/ioc_alarms.rs
+++ src/ioc_alarms.rs
@@ -1,14 +1,21 @@
 use anyhow::Result;
-use tonic::{Request, transport::Channel};
+use tonic::{Request};
 use crate::proto::services::ioc_alarms::*;
 use crate::proto::services::ioc_alarms::ioc_alarms_client::IocAlarmsClient;
 
-pub async fn fetch_ioc_alarm(endpoint: &str, pv_name: String) -> Result<IocAlarmsResponse> {
+pub async fn fetch_ioc_alarm(
+    endpoint: &str,
+    pv_name: String,
+) -> Result<IocAlarmsResponse> {
     let mut client = IocAlarmsClient::connect(endpoint.to_string()).await?;
 
     let req = Request::new(IocAlarmsRequest { pv_name });
 
-    let resp = client.get_ioc_alarms(req).await?.into_inner();
+    let resp = client
+        .get_ioc_alarms(req)
+        .await?
+        .into_inner();
 
     Ok(resp)
 }
+
