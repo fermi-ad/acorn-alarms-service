28e5ecdd69230b5741580ad0c038a51b239f8bf2
Jacob Curley <jcurley@fnal.gov>
2026-08-27 14:42:12 -0500

    Update dependencies and workflows (#108)
    * Update dependencies and workflows
    
    * Exclude tests from coverage
    
    * Use consistent imports

--- src/adapters/acnet_hydration.rs
+++ src/adapters/acnet_hydration.rs
@@ -1,7 +1,6 @@
 //! Startup hydration for AEOLUS-managed ACNET state.
 
-use rust_grpc_lib::pool;
-use tonic::transport::Channel;
+use rust_grpc_lib::auth::ForwardedToken;
 
 use crate::{
     model::key::Key,
@@ -17,8 +16,10 @@ use crate::{
 /// The `host` parameter is expected to be in a format accepted by [`Channel`]. Namely,
 /// it should look like a URI (http(s)://subdomain.domain:port).
 pub async fn load_acnet_hydration(host: String) -> Result<HydratedStatuses, HydrationError> {
-    let mut aeolus_proxy: AeolusProxyClient<Channel> =
-        pool::get(&host).map_err(HydrationError::AeolusProxyConnectionFailed)?;
+    // TODO: Replace with a service token provider when service credentials have been set up
+    let provider = ForwardedToken::new("");
+    let mut aeolus_proxy = AeolusProxyClient::from_endpoint_with_provider(&host, provider)
+        .map_err(HydrationError::AeolusProxyConnectionFailed)?;
 
     let response = aeolus_proxy
         .get_alarms_snapshot(Empty {})
